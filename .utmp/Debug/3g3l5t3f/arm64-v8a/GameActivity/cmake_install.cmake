010371a438d950d328a9d54a26bb9a5bb404cf26
Ahmad A. Fouad <a.adel.dev@gmail.com>
2025-03-06 09:42:22 -0700

    replaced XR Poke Follow affordance fill with the non-fill one for UI buttons

--- .utmp/Debug/3g3l5t3f/arm64-v8a/GameActivity/cmake_install.cmake
+++ .utmp/Debug/3g3l5t3f/arm64-v8a/GameActivity/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Repos/Unity/VirtualPet_V2/Library/PramData/AndroidApplicationController/Cache/com.HXI.VirtualPetV2/gradle-project/unityLibrary/src/main/cpp/GameActivity
+# Install script for directory: C:/Repos/Unity/VirtualPet_V2/Library/Bee/Android/Prj/IL2CPP/Gradle/unityLibrary/src/main/cpp/GameActivity
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
